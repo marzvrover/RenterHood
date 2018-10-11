9530054ba7545339c623903fde16d4df739c2642
Whitney Carrier <>
2018-10-10 21:37:22 -0500

    Adding card view of items to search page.

--- resources/views/UserProfile.blade.php
+++ resources/views/UserProfile.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
-<div class="row">
+<div class="row" style="margin-left: 10px">
     <div class="col-md-7 ">
         <div class="panel panel-default">
             <div class="panel-body">
@@ -22,9 +22,9 @@
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label for="staticEmail" class="col-md-2 col-form-label">Zipcode</label>
+                        <label for="staticEmail" class="col-md-2 col-form-label">Postal Code</label>
                         <div class="col-sm-10">
-                            <input type="text" readonly="" class="form-control-plaintext" id="zipcode" value="61387">
+                            <input type="text" readonly="" class="form-control-plaintext" id="postalCode" value="61387">
                         </div>
                     </div>
                     <div class="form-group row">
--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -12,21 +12,113 @@
                             <option value="1">Postal Code</option>
                             <option value="2">Keywords</option>
                         </select>
-
-
-
-                        {{--<button class="btn btn-outline-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">All</button>
-                        <div class="dropdown-menu">
-                            <a class="dropdown-item" href="#">Postal Code</a>
-                            <a class="dropdown-item" href="#">Keywords</a>
-                        </div>--}}
                     </div>
                     <input type="text" class="form-control" id="search-terms" placeholder="Search&hellip;">
                     <div class="input-group-append">
-                        <button class="btn">Search</button>
+                        <button style="border:1px solid #cbc8d0" class="btn">Search</button>
                     </div>
                 </div>
             </form>
         </div>
     </div>
+    <div class="container" style="padding: 30px">
+         <div class="row">
+            <div class="col-md-4">
+              <div class="card mb-4 box-shadow">
+                <img class="card-img-top" data-src="#" alt="Item For Sale">
+                <div class="card-body">
+                    <h5>Carpet Steamer</h5>
+                    <h6>Postal Code: 68102</h6>
+                    <p class="card-text">Carpet steamer that has only been used a few times. Rent for a week at a time.</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <button type="button" class="btn btn-sm btn-outline-primary">View</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+             <div class="col-md-4">
+                 <div class="card mb-4 box-shadow">
+                     <img class="card-img-top" data-src="" alt="Item For Sale">
+                     <div class="card-body">
+                         <h5>Rider Lawn Mower</h5>
+                         <h6>Postal Code: 68102</h6>
+                         <p class="card-text">Rider lawn more for rent. Must have a trailer to pick it up. May need gas before using.</p>
+                         <div class="d-flex justify-content-between align-items-center">
+                             <div class="btn-group">
+                                 <button type="button" class="btn btn-sm btn-outline-primary">View</button>
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+
+             <div class="col-md-4">
+                 <div class="card mb-4 box-shadow">
+                     <img class="card-img-top" data-src="" alt="Item For Sale">
+                     <div class="card-body">
+                         <h5>Chainsaw</h5>
+                         <h6>Postal Code: 68102</h6>
+                         <p class="card-text">STIHL brand chainsaw. Works like a charm. Will be full of gas and oil when picked up.</p>
+                         <div class="d-flex justify-content-between align-items-center">
+                             <div class="btn-group">
+                                 <button type="button" class="btn btn-sm btn-outline-primary">View</button>
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+
+             <div class="col-md-4">
+                 <div class="card mb-4 box-shadow">
+                     <img class="card-img-top" data-src="" alt="Item For Sale">
+                     <div class="card-body">
+                         <h5>Sewing Machine</h5>
+                         <h6>Postal Code: 68102</h6>
+                         <p class="card-text">Brand new sewing machine out of the box. Willing to rent out for long periods of time.</p>
+                         <div class="d-flex justify-content-between align-items-center">
+                             <div class="btn-group">
+                                 <button type="button" class="btn btn-sm btn-outline-primary">View</button>
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+
+             <div class="col-md-4">
+                 <div class="card mb-4 box-shadow">
+                     <img class="card-img-top" data-src="" alt="Item For Sale">
+                     <div class="card-body">
+                         <h5>Snow Blower</h5>
+                         <h6>Postal Code: 68102</h6>
+                         <p class="card-text">It's almost that time of year again. For rent for a day at a time.</p>
+                         <div class="d-flex justify-content-between align-items-center">
+                             <div class="btn-group">
+                                 <button type="button" class="btn btn-sm btn-outline-primary">View</button>
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+             <div class="col-md-4">
+                 <div class="card mb-4 box-shadow">
+                     <img class="card-img-top" data-src="" alt="Item For Sale">
+                     <div class="card-body">
+                         <h5>Dewalt Drill</h5>
+                         <h6>68102</h6>
+                         <p class="card-text">Dewalt power drill. Battery and charger included when rented.</p>
+                         <div class="d-flex justify-content-between align-items-center">
+                             <div class="btn-group">
+                                 <button type="button" class="btn btn-sm btn-outline-primary">View</button>
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+
+         </div>
+    </div>
+
 @endsection
