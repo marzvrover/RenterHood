3e5f04d56c428eeb6925292ddd81b5b99c4931d5
Matthew Dooley <Matthew@Dooley.xyz>
2018-10-12 13:33:20 -0500

    Add meta viewport and charset

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,6 +2,9 @@
 <html lang="{{ app()->getLocale() }}">
     <head>
         <title>{{ env('APP_NAME') }}</title>
+
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
