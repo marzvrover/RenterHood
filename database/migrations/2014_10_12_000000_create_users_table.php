430ae1a675fa10e041ba612bfba30ea60cd13eb3
Matthew Dooley <Matthew@Dooley.xyz>
2018-10-12 11:28:31 -0500

    Add postal_code and social media handle db support

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,10 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('postal_code');
+            $table->string('facebook')->nullable();
+            $table->string('twitter')->nullable();
+            $table->string('github')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
