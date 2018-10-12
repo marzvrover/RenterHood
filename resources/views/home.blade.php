68385794c6d4001745306d315697a572b3c8dfe7
Matthew Dooley <Matthew@Dooley.xyz>
2018-10-12 12:21:07 -0500

    Update card styling

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,8 +4,8 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+            <div class="card border-secondary">
+                <div class="card-header bg-secondary text-white">Dashboard</div>
 
                 <div class="card-body">
                     @if (session('status'))
