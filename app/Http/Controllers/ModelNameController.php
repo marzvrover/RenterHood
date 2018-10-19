d074cc2c64527c18848aadafe5f878f4f7a9cc01
Maria <mstathopoulos@unomaha.edu>
2018-10-19 18:52:25 -0500

    Created model for search function

---
+++ app/Http/Controllers/ModelNameController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\ModelName;
+use Illuminate\Http\Request;
+
+class ModelNameController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\ModelName  $modelName
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ModelName $modelName)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\ModelName  $modelName
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ModelName $modelName)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\ModelName  $modelName
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ModelName $modelName)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\ModelName  $modelName
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ModelName $modelName)
+    {
+        //
+    }
+}
