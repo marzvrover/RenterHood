f4f371d2195fa8cf195e874552ddb588fabe9899
Maria <mstathopoulos@unomaha.edu>
2018-10-22 15:00:48 -0500

    debug item factory

--- database/factories/ItemFactory.php
+++ database/factories/ItemFactory.php
@@ -2,12 +2,12 @@
 
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(App\Item::class, function (Faker $faker) {
     return [
         'user_id' => 1,
         'name' => $faker->word . ' ' . $faker->word,
         'description' => $faker->paragraph,
-        'postal_code' => $faker->mt_rand(10000, 99999),
+        'postal_code' => mt_rand(10000, 99999),
         'price' => mt_rand(0,100000000),
         'picture' => 'img/stock_item.png',
     ];
