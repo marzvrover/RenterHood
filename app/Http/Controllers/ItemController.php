958e4a2ff8c752704ba2b14f6afa433b2c30ccc9
Maria <mstathopoulos@unomaha.edu>
2018-11-19 15:11:16 -0600

    Fix picture storage

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -53,7 +53,7 @@ class ItemController extends Controller
 
         // Stores image
         if ($request->hasFile('picture')) {
-            $validatedData['picture'] = $request->file('picture')->store('items-picture', ['disk' => 'public']);
+            $validatedData['picture'] = 'storage/' . $request->file('picture')->store('items-picture', ['disk' => 'public']);
         } else {
             $validatedData['picture'] = Item::$defaultImage;
         }
