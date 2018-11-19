ee5b545e5e8afbf04075e49b89f35b35d06f6026
Whitney Carrier <wcarrier@unomaha.edu>
2018-11-19 12:52:52 -0600

    Added profile, inventory, and add item buttons to dashboard on the homepage.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card border-secondary">
-                <div class="card-header bg-secondary text-white">Dashboard</div>
+                <div class="card-header bg-secondary text-white" style="font-size: large">Dashboard</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,15 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    <p style="font-size: medium">You are logged in!</p>
+
+                    <div style="margin-top: 30px" class="text-center">
+
+                        <a role="button" class="btn btn-lg btn-primary @if(request()->route()->getName() == 'add') active @endif" href="{{ route('add') }}">Add Item</a>
+                        <a role="button" class="btn btn-lg btn-primary @if(request()->route()->getName() == 'inventory') active @endif" href="{{ route('inventory') }}">View Inventory</a>
+                        <a role="button" class="btn btn-lg btn-primary @if(request()->route()->getName() == 'users.show') active @endif" href="{{ route('users.show', Auth::user()) }}">View Profile</a>
+
+                    </div>
                 </div>
             </div>
         </div>
