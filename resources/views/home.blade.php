22c4150884dec65567ecc62ac6db164d9b3bb993
Whitney Carrier <wcarrier@unomaha.edu>
2018-11-25 20:14:12 -0600

    Adding Items for Approval to dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,15 +14,44 @@
                         </div>
                     @endif
 
-                    <p style="font-size: medium">You are logged in!</p>
-
-                    <div style="margin-top: 30px" class="text-center">
+                    <div style="margin-top: 10px" class="text-center">
 
                         <a role="button" class="btn btn-lg btn-primary" href="{{ route('items.create') }}">Add Item</a>
                         <a role="button" class="btn btn-lg btn-primary" href="{{ route('inventory') }}">View Inventory</a>
                         <a role="button" class="btn btn-lg btn-primary" href="{{ route('users.show', Auth::user()) }}">View Profile</a>
 
                     </div>
+
+                    <div style="margin-top: 50px">
+                        <h5>Items for Approval</h5>
+                        <table id="ApprovalItems">
+                            <tr>
+                                <th>Item Name</th>
+                                <th>Price</th>
+                                <th>Description</th>
+                                <th>User Requesting</th>
+                                <th>Accept/Deny</th>
+                            </tr>
+                                <td>
+                                    Carpet Steamer
+                                </td>
+                                <td>
+                                    $20.00
+                                </td>
+                                <td>
+                                    Lovely carpet steamer.
+                                </td>
+                                <td>
+                                    Whitney Carrier
+                                </td>
+                                <td class="text-center">
+                                    <a role="button" class="btn btn-sm btn-primary" href="#">Accept</a>
+                                    <a role="button" class="btn btn-sm btn-primary" href="#">Deny</a>
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+
                 </div>
             </div>
         </div>
