d41710e95fbf7324aaf433169a1d25664e20eb8c
Whitney Carrier <wcarrier@unomaha.edu>
2018-11-25 22:22:08 -0600

    Fixing h5s to be divs .

--- resources/views/item/show.blade.php
+++ resources/views/item/show.blade.php
@@ -15,18 +15,17 @@
                                 <img class="inventory-image" style="height: 200px; width: 300px;" src="{{ asset($item->picture) }}" data-src="" alt="Image of {{ $item->name }}">
                             </div>
                             <div class="float-left mt-2" style="padding-left: 40px; width: 400px;">
-                                <h5>Price</h5>
+                                <div class="h5">Price</div>
                                 <p>${{ $item->price }}<p>
-                                <h5>Postal Code</h5>
+                                <div class="h5">Postal Code</div>
                                 <p>{{ $item->postal_code }}<p>
-                                <h5>Description</h5>
+                                <div class="h5">Description</div>
                                 <p class="card-text">{{ $item->description }}</p>
-                                <h5>{{ $item->user->name}}'s Details:</h5>
+                                <div class="h5">{{ $item->user->name}}'s Details:</div>
                                 <p>Phone: {{ $item->user->phone_number}}</p>
                                 <p>Email: {{ $item->user->email}}</p>
                                 <button class="btn btn-primary float-right" id="rentButton">Rent</button>
                             </div>
-
                         </div>
                     </div>
                 </div>
