f2c660b757a90aae1fab9fe388084eb81678cace
Matthew Dooley <Matthew@Dooley.xyz>
2018-11-26 14:24:54 -0600

    Add RentRequestsTableSeeder
    Guests will be asked to login instead of given a rent button

--- resources/views/partials/rent-request/rent-btn.blade.php
+++ resources/views/partials/rent-request/rent-btn.blade.php
@@ -2,9 +2,11 @@
     <button class="btn btn-outline-primary float-right disabled">Requested</button>
 @elseif(Auth::user() && Auth::user()->isRenting($item))
     <button class="btn btn-outline-success float-right disabled">Renting</button>
-@else
+@elseif(Auth::user())
     <form action="{{ route('items.rent', $item->id) }}" method="POST">
         @csrf
         <button class="btn btn-primary float-right">Rent</button>
     </form>
+@else
+    <a class="btn btn-outline-primary float-right" href="{{ route('login') }}">Please Login</a>
 @endif
